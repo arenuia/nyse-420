9b0322cb5fedb3fb24c9c44c7523c08a138697f6
arenuia <arenui.anderson@gmail.com>
2018-11-16 09:06:25 -0700

    added model libraries

--- stock_exchange.py
+++ stock_exchange.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
+from keras.layers import Input, LSTM, Dense, Dropout
+from keras.models import Sequential, Model, load_model
+import sys
+from IPython.core.display import display, HTML
 import os
 cwd = os.getcwd()
 
 ### Read in and set data
 fundms = pd.read_csv(os.path.join(cwd, 'Data/fundamentals.csv'))
-pr_split = pd.read_csv(os.path.join(cwd, 'Data/prices-split-adjusted.csv'))
+prices_sp = pd.read_csv(os.path.join(cwd, 'Data/prices-split-adjusted.csv'))
 prices = pd.read_csv(os.path.join(cwd, 'Data/prices.csv'))
 secu = pd.read_csv(os.path.join(cwd, 'Data/securities.csv'))
+
+prices.head()
